45965b105a708b978cf0c2d62ef9ddbf74e921b4
PeterNex14 <peternex42@gmail.com>
2026-01-29 20:47:11 +0800

    add: connecting to rabbit mq and capture SIGINT signal"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection_string := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connection_string)
+	if err != nil {
+		fmt.Println("Something wrong when connecting with RabbitMQ server")
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Successfuly connecting to RabbitMQ server")
+
+	signChan := make(chan os.Signal, 1)
+	signal.Notify(signChan, os.Interrupt)
+	<-signChan
+	fmt.Println()
+	fmt.Println("RabbitMQ connection close")
+
 }
